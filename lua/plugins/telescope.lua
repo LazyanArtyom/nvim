4f4f4a8db0cd3f40dbf9505e35ad8f02fd6e5e46
LazyanArtyom <artyomlazyan@gmail.com>
2024-10-26 14:10:08 +0400

    Updated telescope find_files config.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,7 +15,13 @@ return {
           "--smart-case",
           "--hidden",
         },
+        file_ignore_patterns = {"node_modules"},
       },
+      pickers = {
+        find_files = {
+            find_command = { "find", ".", "-type", "f", "-not", "-path", "./.git/*", "-not", "-path", "./.cache/*" }
+        }
+      }
     },
   },
 }
